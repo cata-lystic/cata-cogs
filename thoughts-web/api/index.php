f249575556ad5c08e3b5f975241712689b7a6c47
revanced-web-builder <ntqtmslf@protonmail.com>
2023-03-02 03:12:05 -0900

    completely separate API from website

--- thoughts-web/api/index.php
+++ thoughts-web/api/index.php
@@ -93,7 +93,7 @@ class Config {
             $_SESSION['versions'] = $this->versions;
     
         } else {
-            require(__DIR__."/app/config.php");
+            require("config.php");
 
             // Make sure owner has set valid tokens in config.php )
             $checkToken = $this->token($set['token'], 'valid');
@@ -231,7 +231,7 @@ class Config {
             // As far as I know, to do this we have to rewrite the config.php file each time.
             // This will loop through the defaults to get the keys and descriptions to remake the file
             // The user's current config settings will be saved but the requested key will be overwritten
-            require("app/config.php"); // old config settings will be in $set
+            require("config.php"); // old config settings will be in $set
 
             // Make sure this var is an actual setting
             if (!isset($set[$key1][$key2]) || !isset($_SESSION[$key1][$key2])) return "ERROR: `{$key1} {$key2}` is not a valid setting";
@@ -279,7 +279,7 @@ class Config {
                 }
             }
             
-            Files::write("app/config.php", $result);
+            Files::write("config.php", $result);
             return "OK";
         }
 
@@ -431,7 +431,7 @@ class api extends config {
 
         if ($this->api['create'] == 0 && $this->isMod($authorID) == false) die("Post creation is currently disabled");
 
-        $data = Files::read("app/thoughts.json");
+        $data = Files::read("thoughts.json");
         if (!is_array($data)) $data = []; // Create data array if there are no msgs
         $total = count($data); // total thoughts
 
@@ -489,7 +489,7 @@ class api extends config {
 
         // Add this to the thoughts.json
         $data[$nextID] = array("msg" => $cMsg, "tag" => $cTag, "author" => str_replace("HASHTAG", "#", $author), "authorID" => $authorID, "timestamp" => time(), "source" => $platform);
-        Files::write("app/thoughts.json", json_encode($data, JSON_PRETTY_PRINT));
+        Files::write("thoughts.json", json_encode($data, JSON_PRETTY_PRINT));
         die();
 
     }
@@ -511,7 +511,7 @@ class api extends config {
             die("Missing id, deleter, deleterID, or reason");
 
         // Load thoughts data
-        $data = Files::read("app/thoughts.json");
+        $data = Files::read("thoughts.json");
         if (!is_array($data)) $data = []; // Create data array if there are no msgs
         $total = count($data); // total thoughts
         if ($total == 0) return "There are no thoughts to delete";
@@ -539,7 +539,7 @@ class api extends config {
 
             $data[$id]['deleteReason'] = $reason;
             if ($wipe == 1) $data[$id]['msg'] = "[WIPED]";
-            Files::write("app/thoughts.json", json_encode($data, JSON_PRETTY_PRINT));
+            Files::write("thoughts.json", json_encode($data, JSON_PRETTY_PRINT));
             $delMethod = ($wipe != 1) ? "deleted" : "wiped";
             echo "Post #{$id} {$delMethod}";
 
@@ -562,7 +562,7 @@ class api extends config {
         if ($checkToken !== true) die($checkToken);
 
         // Get Settings and Thoughts
-        $data = Files::read("app/thoughts.json");
+        $data = Files::read("thoughts.json");
         if (!is_array($data)) $data = []; // Create data array if there are no msgs
 
         // Get possible queries
@@ -726,7 +726,7 @@ class api extends config {
             // Make sure tag exists
             if (!is_array($tags)) die("Tag doesn't exist");
 
-            $data = Files::read("app/thoughts.json");
+            $data = Files::read("thoughts.json");
             if (!is_array($data)) $data = []; // Create data array if there are no posts
 
             // Rename tag
@@ -740,7 +740,7 @@ class api extends config {
                     if ($val['tag'] == $tag) $data[$key]['tag'] = $tagRename;
                 }
                 $this->set('api', 'tags', $this->api['tags']);
-                Files::write("app/thoughts.json", json_encode($data, JSON_PRETTY_PRINT));
+                Files::write(__DIR__."thoughts.json", json_encode($data, JSON_PRETTY_PRINT));
                 echo "Tag `$tag` renamed to `$tagRename`";
             }
 
@@ -791,145 +791,6 @@ class api extends config {
     }
 
 }
-
-
-// View class to get HTML for search, api box, create box, etc
-class view {
-
-    public function search($args=[]) {
-
-        if ($_SESSION['web']['search'] != 1) return false;
-
-        $s = $args['s'] ?? ''; // Search query
-        $limit = $args['limit'] ?? '';
-        $quotes = $args['quotes'] ?? '';
-        $showID = $args['showID'] ?? '';
-        $shuffle = $args['shuffle'] ?? '';
-        $shuffleChecked = ($shuffle == 1) ? "checked" : null;
-        $showIDChecked = ($showID == 1) ? "check" : null;
-        $submitVisible = ($_SESSION['web']['js'] != 1) ? "<input id='searchSubmit' type='submit' value='Search' />":null; // Submit button only needs to be shown if javascript is disabled
-        $searchVisible = ($_SESSION['web']['searchVisible'] == 1) ? "block":"none";
-        return "
-        <div id='search' style='display: {$searchVisible}'>
-        <form id='searchForm' method='get' action='index.php'>
-            <p><input type='text' id='searchbox' name='q' placeholder='Search...' value='{$s}' /><p>
-            <p><label>Limit: <input type='number' id='searchLimit' name='limit' value='{$limit}' size='4' /></label> <label>Quotes: <input type='text' id='searchQuotes' name='quotes' value='{$quotes}' size='3'></label></p>
-            <p><label><input type='checkbox' id='searchShuffle' name='shuffle' value='1' {$shuffleChecked} /> Shuffle</label> <label><input type='checkbox' id='searchShowID' name='showID' {$showIDChecked} /> Show ID</label></p> {$submitVisible}
-            <input type='hidden' id='searchJS' name='js' value='0' />
-            <input type='hidden' id='showAPI' name='showAPI' value='1' />
-            <input type='hidden' id='showSearch' name='showSearch' value='1' />
-        </form>
-        </div>";
-    }
-    
-    // Create Box
-    public function create() {
-        
-        if ($_SESSION['web']['create'] != 1 || $_SESSION['api']['create'] != 1) return false;
-
-        $createVisible = ($_SESSION['web']['createVisible'] == 1) ? "block":"none";
-        $createForm = "
-        <div id='create' style='display: {$createVisible}'>
-        <form id='createForm' method='get' action='index.php'>
-            <input type='hidden' name='q' value='create' />
-            <h1>Create Thought</h1>
-            <p><input type='text' id='createUser' name='author' placeholder='Username' value='' /><p>
-            <p><input type='text' id='createUserID' name='authorID' placeholder='User ID' value='' /><p>
-            <p><select id='createTag' name='tag'>";
-            $tags = $_SESSION['api']['tags'];
-            $createForm .= "<option value='".strtolower($_SESSION['api']['tagDefault'])."'>".ucfirst($_SESSION['api']['tagDefault'])."</option>"; // immediately show the default tag
-            sort($tags); // sort tags alphabetically
-            foreach ($tags as $name) {
-                if ($name == $_SESSION['api']['tagDefault']) continue; // don't list the default tag
-                $createForm .= "<option value='{$name}'>".ucfirst($name)."</option>";
-            }
-            $createForm .= "</select></p>
-            <p><input type='text' id='createThought' name='msg' placeholder='Message' value='' /><p>
-            <p><input type='submit' id='createSubmit' name='createSubmit' value='Submit Thought' /></p>
-            <input type='hidden' id='createTrigger' name='create' value='1' />
-        </form>
-        </div>";
-        echo $createForm;
-
-    }
-
-    // API Info Box
-    public function infoBox() {
-        if ($_SESSION['web']['info'] != 1) return false;
-        $visible = ($_SESSION['web']['infoVisible'] == 1) ? "block":"none"; // Default visibility
-        $d = $_SESSION['api']['url'];
-        echo "
-        <div id='info' style='display: {$visible}'>
-            <h1>API</h1>
-            <p>Random Thought<br />
-            <a href='{$d}'>{$d}</a></p>
-            <p>Thought List<br />
-            <a href='{$d}?s=list'>{$d}?s=list</a></p>
-            <p>Link to Thought List<br />
-            <a href='{$d}?s=list&platform=discord'>{$d}?s=list&platform=discord</a></p>
-            <p>Thought by ID #<br />
-            <a href='{$d}?s=25'>{$d}?s=25</a> (ID #25)</p>
-            <p>Thought by word search<br />
-            <a href='{$d}?s=multiple word search&limit=3'>{$d}?s=multiple words&limit=3&shuffle=1</a></p>
-            <p>Other flags<br />
-            &js=1&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Javascript on web page<br />
-            &showID=0&nbsp;&nbsp;&nbsp;&nbsp;Show ID of thought<br />
-            &amp;quotes=&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Mark to put around each thought (single, double, none)<br />
-            &breaks=0&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Return &lt;br /&gt; instead of \\n\\r</p>
-        </div>";
-    }
-    
-    public static function footer() {
-        $ret = array(null,null,null,null); // Return variables
-
-        if ($_SESSION['web']['create'] == 1)
-            $ret[0] = "<a href='#' data-toggle='#create' class='divToggle'>Create</a>&nbsp;&nbsp;&nbsp;";
-
-        if ($_SESSION['web']['search'] == 1)
-            $ret[1] = "<a href='#' data-toggle='#search' class='divToggle'>Search</a>&nbsp;&nbsp;&nbsp;";
-
-        if ($_SESSION['web']['info'] == 1)
-            $ret[2] = "<a href='#' data-toggle='#info' class='divToggle'>API</a>&nbsp;&nbsp;&nbsp;";
-
-        if ($_SESSION['web']['github'] == 1)
-            $ret[3] = "<a href='https://github.com/cata-lystic/redbot-cogs/tree/main/thoughts' target='_blank'>GitHub</a>&nbsp;&nbsp;&nbsp;";
-
-        if ($_SESSION['web']['versionVisible'] == 1)
-            $ret[4] = "<span title='API Version: ".$_SESSION['versions']['api']."'>Thoughts {$_SESSION['versions']['web']}</span>";
-
-        return "
-        <div id='footer'>
-            <div id='footerContent'>
-                {$ret[0]}{$ret[1]}{$ret[2]}{$ret[3]}{$ret[4]}
-            </div>
-        </div>";
-    }
-
-    // Detect which Javascript and jQuery file to use (or not use)
-    public function js() {
-
-        if ($_SESSION['web']['js'] != 1 || isset($_REQUEST['js']) && $_REQUEST['js'] == 0) return false;
-        $jq = strtolower($_SESSION['web']['jquery']);
-        $jqVer = "3.6.3";
-
-        // Supported CDNs
-        $cdns = array(
-        "local" => "app/jquery-{$jqVer}.min.js",
-        "google" => "https://ajax.googleapis.com/ajax/libs/jquery/{$jqVer}/jquery.min.js",
-        "jquery" => "https://code.jquery.com/jquery-{$jqVer}.min.js",
-        "microsoft" => "https://ajax.aspnetcdn.com/ajax/jQuery/jquery-{$jqVer}.min.js",
-        "cdnjs" => "https://cdnjs.cloudflare.com/ajax/libs/jquery/{$jqVer}/jquery.min.js",
-        "jsdelivr" => "https://cdn.jsdelivr.net/npm/jquery@{$jqVer}/dist/jquery.min.js");
-
-        // If $jq is in the CDNs array, use it. If not, use what URL the user hopeufully supplied
-        $jquery = (isset($cdns[$jq])) ? $cdns[$jq] : $jq;
-
-        return "
-        <script src='{$jquery}'></script>
-        <script src='app/thoughts.js'></script>";
-    }
-
-  }
 
 // Misc functions
 class tools {
--- thoughts-web/index.php
+++ thoughts-web/index.php
@@ -6,10 +6,10 @@ ini_set("memory_limit", 512 * 1024 * 1024); // Set memory limit to 512mb so PHP 
 error_reporting(E_ALL);
 
 $web = true; // Let API know this is the website
-require_once("api.php");
+require_once("api/index.php");
 
 // Get Settings and Thoughts
-$data = Files::read("app/thoughts.json");
+$data = Files::read("thoughts.json");
 if (!is_array($data)) $data = []; // Create data array if there are no msgs
 
 // Get possible queries
@@ -36,7 +36,7 @@ echo "<head>
 
 <style>:root { --bg-color: {$config->web['backgroundColor']}; --accent-color: {$config->web['accentColor']}; --font-color: {$config->web['fontColor']}; --accent-radius: {$config->web['accentRadius']}; }</style>
 
-<link rel='stylesheet' href='app/thoughts.css'>
+<link rel='stylesheet' href='assets/thoughts.css'>
 </head>
 <body>
 
@@ -58,3 +58,142 @@ echo $view->search(array('s'=>$s, 'limit'=>$limit, 'quotes'=>$quotes, 'shuffle'=
 echo $view->footer(); // Footer  
 
 echo $view->js(); // Javascript, if enabled
+
+
+// View class to get HTML for search, api box, create box, etc
+class view {
+
+  public function search($args=[]) {
+
+      if ($_SESSION['web']['search'] != 1) return false;
+
+      $s = $args['s'] ?? ''; // Search query
+      $limit = $args['limit'] ?? '';
+      $quotes = $args['quotes'] ?? '';
+      $showID = $args['showID'] ?? '';
+      $shuffle = $args['shuffle'] ?? '';
+      $shuffleChecked = ($shuffle == 1) ? "checked" : null;
+      $showIDChecked = ($showID == 1) ? "check" : null;
+      $submitVisible = ($_SESSION['web']['js'] != 1) ? "<input id='searchSubmit' type='submit' value='Search' />":null; // Submit button only needs to be shown if javascript is disabled
+      $searchVisible = ($_SESSION['web']['searchVisible'] == 1) ? "block":"none";
+      return "
+      <div id='search' style='display: {$searchVisible}'>
+      <form id='searchForm' method='get' action='index.php'>
+          <p><input type='text' id='searchbox' name='q' placeholder='Search...' value='{$s}' /><p>
+          <p><label>Limit: <input type='number' id='searchLimit' name='limit' value='{$limit}' size='4' /></label> <label>Quotes: <input type='text' id='searchQuotes' name='quotes' value='{$quotes}' size='3'></label></p>
+          <p><label><input type='checkbox' id='searchShuffle' name='shuffle' value='1' {$shuffleChecked} /> Shuffle</label> <label><input type='checkbox' id='searchShowID' name='showID' {$showIDChecked} /> Show ID</label></p> {$submitVisible}
+          <input type='hidden' id='searchJS' name='js' value='0' />
+          <input type='hidden' id='showAPI' name='showAPI' value='1' />
+          <input type='hidden' id='showSearch' name='showSearch' value='1' />
+      </form>
+      </div>";
+  }
+  
+  // Create Box
+  public function create() {
+      
+      if ($_SESSION['web']['create'] != 1 || $_SESSION['api']['create'] != 1) return false;
+
+      $createVisible = ($_SESSION['web']['createVisible'] == 1) ? "block":"none";
+      $createForm = "
+      <div id='create' style='display: {$createVisible}'>
+      <form id='createForm' method='get' action='index.php'>
+          <input type='hidden' name='q' value='create' />
+          <h1>Create Thought</h1>
+          <p><input type='text' id='createUser' name='author' placeholder='Username' value='' /><p>
+          <p><input type='text' id='createUserID' name='authorID' placeholder='User ID' value='' /><p>
+          <p><select id='createTag' name='tag'>";
+          $tags = $_SESSION['api']['tags'];
+          $createForm .= "<option value='".strtolower($_SESSION['api']['tagDefault'])."'>".ucfirst($_SESSION['api']['tagDefault'])."</option>"; // immediately show the default tag
+          sort($tags); // sort tags alphabetically
+          foreach ($tags as $name) {
+              if ($name == $_SESSION['api']['tagDefault']) continue; // don't list the default tag
+              $createForm .= "<option value='{$name}'>".ucfirst($name)."</option>";
+          }
+          $createForm .= "</select></p>
+          <p><input type='text' id='createThought' name='msg' placeholder='Message' value='' /><p>
+          <p><input type='submit' id='createSubmit' name='createSubmit' value='Submit Thought' /></p>
+          <input type='hidden' id='createTrigger' name='create' value='1' />
+      </form>
+      </div>";
+      echo $createForm;
+
+  }
+
+  // API Info Box
+  public function infoBox() {
+      if ($_SESSION['web']['info'] != 1) return false;
+      $visible = ($_SESSION['web']['infoVisible'] == 1) ? "block":"none"; // Default visibility
+      $d = $_SESSION['api']['url'];
+      echo "
+      <div id='info' style='display: {$visible}'>
+          <h1>API</h1>
+          <p>Random Thought<br />
+          <a href='{$d}'>{$d}</a></p>
+          <p>Thought List<br />
+          <a href='{$d}?s=list'>{$d}?s=list</a></p>
+          <p>Link to Thought List<br />
+          <a href='{$d}?s=list&platform=discord'>{$d}?s=list&platform=discord</a></p>
+          <p>Thought by ID #<br />
+          <a href='{$d}?s=25'>{$d}?s=25</a> (ID #25)</p>
+          <p>Thought by word search<br />
+          <a href='{$d}?s=multiple word search&limit=3'>{$d}?s=multiple words&limit=3&shuffle=1</a></p>
+          <p>Other flags<br />
+          &js=1&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Javascript on web page<br />
+          &showID=0&nbsp;&nbsp;&nbsp;&nbsp;Show ID of thought<br />
+          &amp;quotes=&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Mark to put around each thought (single, double, none)<br />
+          &breaks=0&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Return &lt;br /&gt; instead of \\n\\r</p>
+      </div>";
+  }
+  
+  public static function footer() {
+      $ret = array(null,null,null,null); // Return variables
+
+      if ($_SESSION['web']['create'] == 1)
+          $ret[0] = "<a href='#' data-toggle='#create' class='divToggle'>Create</a>&nbsp;&nbsp;&nbsp;";
+
+      if ($_SESSION['web']['search'] == 1)
+          $ret[1] = "<a href='#' data-toggle='#search' class='divToggle'>Search</a>&nbsp;&nbsp;&nbsp;";
+
+      if ($_SESSION['web']['info'] == 1)
+          $ret[2] = "<a href='#' data-toggle='#info' class='divToggle'>API</a>&nbsp;&nbsp;&nbsp;";
+
+      if ($_SESSION['web']['github'] == 1)
+          $ret[3] = "<a href='https://github.com/cata-lystic/redbot-cogs/tree/main/thoughts' target='_blank'>GitHub</a>&nbsp;&nbsp;&nbsp;";
+
+      if ($_SESSION['web']['versionVisible'] == 1)
+          $ret[4] = "<span title='API Version: ".$_SESSION['versions']['api']."'>Thoughts {$_SESSION['versions']['web']}</span>";
+
+      return "
+      <div id='footer'>
+          <div id='footerContent'>
+              {$ret[0]}{$ret[1]}{$ret[2]}{$ret[3]}{$ret[4]}
+          </div>
+      </div>";
+  }
+
+  // Detect which Javascript and jQuery file to use (or not use)
+  public function js() {
+
+      if ($_SESSION['web']['js'] != 1 || isset($_REQUEST['js']) && $_REQUEST['js'] == 0) return false;
+      $jq = strtolower($_SESSION['web']['jquery']);
+      $jqVer = "3.6.3";
+
+      // Supported CDNs
+      $cdns = array(
+      "local" => "assets/jquery-{$jqVer}.min.js",
+      "google" => "https://ajax.googleapis.com/ajax/libs/jquery/{$jqVer}/jquery.min.js",
+      "jquery" => "https://code.jquery.com/jquery-{$jqVer}.min.js",
+      "microsoft" => "https://ajax.aspnetcdn.com/ajax/jQuery/jquery-{$jqVer}.min.js",
+      "cdnjs" => "https://cdnjs.cloudflare.com/ajax/libs/jquery/{$jqVer}/jquery.min.js",
+      "jsdelivr" => "https://cdn.jsdelivr.net/npm/jquery@{$jqVer}/dist/jquery.min.js");
+
+      // If $jq is in the CDNs array, use it. If not, use what URL the user hopeufully supplied
+      $jquery = (isset($cdns[$jq])) ? $cdns[$jq] : $jq;
+
+      return "
+      <script src='{$jquery}'></script>
+      <script src='assets/thoughts.js'></script>";
+  }
+
+}
